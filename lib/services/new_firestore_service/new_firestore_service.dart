26eea0c6f12b5ba9485b7eb7d5c63992174142f7
StrephonTran <taitran.deve@gmail.com>
2023-02-18 15:39:17 +0700

    create a new_firestore_service

---
+++ lib/services/new_firestore_service/new_firestore_service.dart
@@ -0,0 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class UserService {
+  final userId = FirebaseAuth.instance.currentUser!.uid;
+  final userName = FirebaseAuth.instance.currentUser!.displayName;
+  final userEmail = FirebaseAuth.instance.currentUser!.email;
+
+  CollectionReference userRef =
+      FirebaseFirestore.instance.collection('users_test_model');
+
+  // Create a user first
+  Future<void> creatingUserInformation(userName) async {
+    await userRef
+        .doc(userId)
+        .set({'email': userEmail, 'user_id': userId, 'user_name': userName});
+  }
+}
+
+class PostServiceNew {
+  final userId = FirebaseAuth.instance.currentUser!.uid;
+  final userName = FirebaseAuth.instance.currentUser!.displayName;
+
+  CollectionReference userRef =
+      FirebaseFirestore.instance.collection('users_test_model');
+
+  // Creating Post
+
+  // Drafting Post
+
+  // Delete Post
+
+}
